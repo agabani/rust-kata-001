6b703494fcc193568724614140950e2cc8cf1d09
agabani <agabani@live.com>
2020-10-21 17:41:27 +0100

    API integration tests

--- src/graph/api/mod.rs
+++ src/graph/api/mod.rs
@@ -74,3 +74,64 @@ fn sanitise_version(version: &str) -> String {
 
     chars.iter().collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::factory::http_client;
+    use crate::graph::api::dependencies;
+
+    #[actix_rt::test]
+    #[ignore]
+    async fn integration_dependencies() -> Result<(), String> {
+        let client = http_client::new()?;
+
+        let c = dependencies(&client, "time".to_owned(), "0.2.22".to_owned()).await?;
+
+        println!("{:?}", c);
+
+        assert_eq!(c.name, "time");
+        assert_eq!(c.version, semver::Version::new(0, 2, 22));
+
+        let dependencies = c.dependency;
+        assert_eq!(dependencies.len(), 12);
+
+        // concrete
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "const_fn" && d.version == semver::Version::new(0, 4, 2)));
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "standback" && d.version == semver::Version::new(0, 2, 5)));
+
+        // inference
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "libc" && d.version == semver::Version::new(0, 2, 00)));
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "rand" && d.version == semver::Version::new(0, 7, 00)));
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "standback" && d.version == semver::Version::new(0, 2, 00)));
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "stdweb" && d.version == semver::Version::new(0, 4, 00)));
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "serde" && d.version == semver::Version::new(1, 00, 00)));
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "serde_json" && d.version == semver::Version::new(1, 00, 00)));
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "time-macros" && d.version == semver::Version::new(0, 1, 00)));
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "version_check" && d.version == semver::Version::new(0, 9, 00)));
+        assert!(dependencies
+            .iter()
+            .any(|d| d.name == "winapi" && d.version == semver::Version::new(0, 3, 00)));
+
+        Ok(())
+    }
+}
